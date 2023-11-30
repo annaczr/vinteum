b50afac30c74042cb141fa56ad1d1bb0249966ef
MarcusVincius00 <mvrthomazetti@gmail.com>
2023-11-30 10:10:33 -0300

    Resolvendo os conflitos do primeiro commit

--- vinteum/AppDelegate.swift
+++ vinteum/AppDelegate.swift
@@ -2,14 +2,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
-    let viewController = ViewController()
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = viewController
-    window?.makeKeyAndVisible()
-    return true
-   }
+
+    var window: UIWindow?
+      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+      }
+
 }
-	
--- vinteum/ViewController.swift
+++ vinteum/ViewController.swift
@@ -4,33 +4,67 @@ import SnapKit
 class ViewController: UIViewController {
     
     private let backgroundImage = UIImage(named: "HomeBackground")
-
     
-    let text: UILabel = {
-        let text = UILabel()
-        text.text = "vinte-um"
-        text.textColor = .black
-        text.font = UIFont.systemFont(ofSize: 40, weight: .bold)
-        return text
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("new game", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 15
+        return button
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        
-        let backgroundImageView = UIImageView(image: self.backgroundImage)
-        backgroundImageView.contentMode = .scaleAspectFill
-        self.view.addSubview(backgroundImageView)
-        backgroundImageView.snp.makeConstraints{ make in
-            make.edges.equalToSuperview()
+    private let titulo: UILabel = {
+        let titulo = UILabel()
+        titulo.text = "twenty-one"
+        titulo.textColor = .black
+        titulo.textAlignment = .center
+        titulo.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        return titulo
+       }()
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            //Settando titulo da pagina
+            self.view.addSubview(titulo)
+                titulo.snp.makeConstraints{ make in
+                  make.centerX.equalToSuperview()
+                  make.top.equalToSuperview().offset(250)
+                }
+            
+            
+            //Settando a imagem como background
+            let backgroundImageView = UIImageView(image: backgroundImage)
+            backgroundImageView.contentMode = .scaleAspectFill
+            self.view.addSubview(backgroundImageView)
+            
+            backgroundImageView.snp.makeConstraints{ make in
+                make.edges.equalToSuperview()
+            }
+
+            //Settando botão e adicionando funcionalidade
+            self.button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+
+            self.view.addSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            button.snp.makeConstraints{ make in
+                make.width.equalTo(250)
+                make.centerX.equalToSuperview()
+                make.centerY.equalToSuperview().offset(200)
+            }
+            
+          }
+
+        @objc private func didTapButton(){
+            let mainController = MainController()
+//            addChild(mainController)
+//            view.addSubview(mainController.view)
+//            mainController.didMove(toParent: self)
+          navigationController?.pushViewController(mainController, animated: true)
         }
 
-        
-        self.view.addSubview(text)
-        text.snp.makeConstraints{ make in
-            make.centerX.equalToSuperview()
-            make.top.equalToSuperview().offset(250)
-        }
-    }
+
 }
